7520cc6108c2974c66b1f3bed7df220faa7c779a
stephanoumenos <stefano@dechecchi.dev>
2023-08-19 19:55:14 +0200

    add autocompletion to anchor down!

--- main.go
+++ main.go
@@ -50,6 +50,20 @@ func main() {
 				fmt.Println("⚓️ Anchored to", currentDir)
 			}
 		},
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			savedAnchors, err := config.ListSavedAnchors()
+			if err != nil {
+				fmt.Println("Error getting saved anchors:", err)
+				return nil, cobra.ShellCompDirectiveError
+			}
+
+			savedAnchorNames := make([]string, 0, len(savedAnchors))
+			for anchorName := range savedAnchors {
+				savedAnchorNames = append(savedAnchorNames, anchorName)
+			}
+
+			return savedAnchorNames, cobra.ShellCompDirectiveNoFileComp
+		},
 	}
 
 	var cmdUp = &cobra.Command{
@@ -130,8 +144,40 @@ func main() {
 		},
 	}
 
+	var completionCmd = &cobra.Command{
+		Use:   "completion [bash|zsh|fish|powershell]",
+		Short: "Generate completion script",
+		Long: `To load completions:
+Bash:
+
+$ source <(yourprogram completion bash)
+
+
+Zsh:
+
+$ source <(yourprogram completion zsh)
+
+Fish:
+
+$ yourprogram completion fish | source
+`,
+		Args: cobra.ExactValidArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			switch args[0] {
+			case "bash":
+				cmd.Root().GenBashCompletion(os.Stdout)
+			case "zsh":
+				cmd.Root().GenZshCompletion(os.Stdout)
+			case "fish":
+				cmd.Root().GenFishCompletion(os.Stdout, true)
+			case "powershell":
+				cmd.Root().GenPowerShellCompletion(os.Stdout)
+			}
+		},
+	}
+
 	var rootCmd = &cobra.Command{Use: "anchor"}
-	rootCmd.AddCommand(cmdDown, cmdUp, cmdSave, cmdRemove, cmdList, cmdGet)
+	rootCmd.AddCommand(cmdDown, cmdUp, cmdSave, cmdRemove, cmdList, cmdGet, completionCmd)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
