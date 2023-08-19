0003984feaa4b86d4b65d39c8f3ff237f76c0e68
stephanoumenos <stefano@dechecchi.dev>
2023-08-19 18:33:07 +0200

    implement anchor down

--- config/config.go
+++ config/config.go
@@ -1,6 +1,51 @@
 package config
 
-import "os"
+import (
+	"encoding/json"
+	"os"
+)
+
+type config struct {
+	CurrentAnchor *string `json:"current_anchor"`
+}
+
+// Just to avoid calling filepath.Dir to get the directory
+type configPath struct {
+	path string
+	dir  string
+}
+
+func getConfigPath() (*configPath, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	return &configPath{
+		path: home + "/.config/anchor/config.json",
+		dir:  home + "/.config/anchor",
+	}, nil
+}
+
+func saveConfig(config *config) error {
+	configPath, err := getConfigPath()
+	if err != nil {
+		return err
+	}
+
+	err = os.MkdirAll(configPath.dir, 0755)
+	if err != nil {
+		return err
+	}
+
+	marshalledConfig, err := json.Marshal(config)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(configPath.path, marshalledConfig, 0644)
+	return err
+}
 
 func AnchorToPath(path string) error {
 	err := os.Setenv("ANCHOR", path)
@@ -9,5 +54,9 @@ func AnchorToPath(path string) error {
 	}
 
 	// Save to config file
-	return nil
+	return saveConfig(
+		&config{
+			CurrentAnchor: &path,
+		},
+	)
 }
